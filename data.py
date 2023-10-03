5c39d145bc511c4afb7eabd0109c9b42e2b18ddb
John Szwakob III <szwakoj@sunypoly.edu>
2023-10-03 00:55:46 -0400

    Added model, main, data python files

---
+++ data.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2
+#Data preprocessing functions and utilities
+
+#
---
+++ model.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow.keras
+from tensorflow.keras import layers, Model
+import cv2
+
+
+class Crowd_Model(Model):
+    def __init__(sel):
+        super().__init__()
+        self.model = build_model()
+
+    def build_model(self):
+        #build functional model
+
+    def compile(self):
+        super().compile()
+
+    def train_step(self, gen_data):
+        #either custom single train_step or use custom tf.model.fit() to train
+
+
